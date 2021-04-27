8fef7ed2af1e557edfab5cd87691b530e2681f0c
Miebi Iyeyemi <iyeyemim@gmail.com>
2021-04-27 12:08:06 +0200

    Change initial value of data

--- src/Blogs.js
+++ src/Blogs.js
@@ -2,11 +2,7 @@ import React, { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Blogs = (props) => {
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-  ]);
+  const [blogs, setBlogs] = useState(null);
 
   const handleDelete = (id) => {
     setBlogs((prev) => prev.filter(blog => blog.id !== id));
